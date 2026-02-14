ee7f0bb9c2d915fe394fb015f8c81e0051a0be1a
Mosu <180250518+Mosuzzzz@users.noreply.github.com>
2026-02-14 22:10:31 +0700

    Delete Avengers-Assemble-client/src/environments directory

--- Avengers-Assemble-client/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    baseUrl: '',
-        emailjs: {
-        serviceId: 'service_olxe6tp',
-        templateId: 'template_2p2t3s6',
-        publicKey: 'CfRsKnMVCuXbj_EeC'
-    }
-}
