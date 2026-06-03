f0359b1541fde8bcc3cf5fe5932b6ff591c33253
Mosuzzzz <180250518+Mosuzzzz@users.noreply.github.com>
2026-06-03 13:49:05 +0700

    feat: integrate diesel_migrations for database management and update migration directory path

--- server/src/infrastructure/database/postgresql_connection.rs
+++ server/src/infrastructure/database/postgresql_connection.rs
@@ -3,11 +3,19 @@ use diesel::{
     PgConnection,
     r2d2::{ConnectionManager, Pool},
 };
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("src/infrastructure/database/migrations");
 
 pub type PgPoolSquad = Pool<ConnectionManager<PgConnection>>;
 
 pub fn establish_connection(database_url: &str) -> Result<PgPoolSquad> {
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = Pool::builder().build(manager)?;
+
+    let mut conn = pool.get()?;
+    conn.run_pending_migrations(MIGRATIONS)
+        .expect("Failed to run database migrations");
+
     Ok(pool)
 }
