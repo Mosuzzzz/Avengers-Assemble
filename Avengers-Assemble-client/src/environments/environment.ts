0bef6ef8af4dede6a89d3bb42bee93c19f87091b
Mosuzzzz <180250518+Mosuzzzz@users.noreply.github.com>
2026-06-03 14:37:53 +0700

    feat: update baseUrl in environment configuration for API integration

--- Avengers-Assemble-client/src/environments/environment.ts
+++ Avengers-Assemble-client/src/environments/environment.ts
@@ -1,6 +1,5 @@
-// TODO: replace with your Render API URL, e.g. 'https://avengers-assemble-api.onrender.com'
 export const environment = {
-    baseUrl: '' ,
+    baseUrl: 'https://avengers-assemble-f6dg.onrender.com',
     emailjs: {
         serviceId: 'service_olxe6tp',
         templateId: 'template_2p2t3s6',
